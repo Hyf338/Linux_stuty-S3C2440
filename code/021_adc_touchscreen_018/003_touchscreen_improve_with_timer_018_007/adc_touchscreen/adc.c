e48acf1ec191f6de36308e3a9b7e0c885188951d
Hyf338 <hyifen@outlook.com>
2020-03-01 13:51:07 +0800

    test : http

---
+++ code/021_adc_touchscreen_018/003_touchscreen_improve_with_timer_018_007/adc_touchscreen/adc.c
@@ -0,0 +1,30 @@
+#include "../s3c2440_soc.h"
+
+void adc_init(void)
+{
+	/* [15]  ：ECFLG        ，1 = End of A/D conversion
+	 * [14]  : PRSCEN      ,1 = A/D converter prescaler enable
+	 * [13:6]: PRSCVL      ,adc clk = PCK / (PRSCVL + 1)
+	 * [5:3] : SEL_MUX     ,000 = AIN 0
+	 * [2]   : STDBM       ,1 = Standby mode
+	 * [1]   : READ_ START ,0 = Disable start by read operation
+	 * [0]   : ENABLE_START,1 = A/D conversion starts and this bit is cleared after the startup.
+	*/
+	ADCCON = (1<<14) | (49<<6) | (0<<3) ;
+
+	ADCDLY = 0xff;
+	
+}
+
+
+int adc_read_ain0(void)
+{
+	/* 启动adc */
+	ADCCON |= (1<<0);
+	
+	while(ADCCON & (1<<15));	/* 等待ADC结束 */
+
+	return ADCDAT0 & 0x3ff;
+}
+
+
---
+++ code/021_adc_touchscreen_018/003_touchscreen_improve_with_timer_018_007/adc_touchscreen/adc.h
@@ -0,0 +1,11 @@
+
+#ifndef _ADC_H
+#define _ADC_H
+
+void adc_init(void);
+int adc_read_ain0(void);
+
+
+#endif
+
+
